fa10464697c3020f6a7c418fa394a68c54a51675
Changgeng Zhao <changgeng@nyu.edu>
2019-02-23 21:13:11 -0500

    small change

--- server/server.go
+++ server/server.go
@@ -57,7 +57,8 @@ func (s *MineServer) handleConnections(w http.ResponseWriter, r *http.Request) {
 	// Upgrade initial GET request to a websocket
 	ws, err := s.upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	// Make sure we close the connection when the function returns
 	defer ws.Close()
